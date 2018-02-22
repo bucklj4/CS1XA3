9504487b1d9d09f8992bf07bc2d92a52cc834918
Jack Joseph Buckley <bucklj4@mills.cas.mcmaster.ca>
2018-02-22 14:11:22 -0500

    Programmed up to Question 3

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -16,3 +16,10 @@ then
 else
 	echo "The local repo is NOT up to date with the local repo."
 fi
+
+# Question 2
+
+(git diff) >> changes.log
+
+# Question 3
+(grep -r -h "#TODO" --exclude="ProjectAnalyze.sh" --exclude="changes.log" --exclude="todo.log" .) >> todo.log
